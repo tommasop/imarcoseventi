37214d1e09eee6782658bb981fd7a7fba0059fd4
Tommaso Patrizi <tommasop@thinkingco.de>
2025-03-13 17:51:21 +0100

    updated astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare(),
 });
